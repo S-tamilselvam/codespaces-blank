28f95b38bd4b3a2f93f9b7a15eb46c0095eb84ac
Tamilselvam S <132042629+S-tamilselvam@users.noreply.github.com>
2023-05-15 15:03:37 +0000

    c

--- automator.py
+++ automator.py
@@ -1,12 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+"""Hello"""
+# pylint: disable=C0103,C0114
+
+import os
 from time import sleep
 from urllib.parse import quote
-import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -15,6 +19,8 @@ options.add_argument("--user-data-dir=/var/tmp/chrome_user_data")
 
 os.system("")
 os.environ["WDM_LOG_LEVEL"] = "0"
+
+
 class style():
     BLACK = '\033[30m'
     RED = '\033[31m'
@@ -26,6 +32,7 @@ class style():
     WHITE = '\033[37m'
     UNDERLINE = '\033[4m'
     RESET = '\033[0m'
+
 
 print(style.BLUE)
 print("**********************************************************")
@@ -51,11 +58,12 @@ message = quote(message)
 numbers = []
 f = open("numbers.txt", "r")
 for line in f.read().splitlines():
-	if line.strip() != "":
-		numbers.append(line.strip())
+    if line.strip() != "":
+        numbers.append(line.strip())
 f.close()
-total_number=len(numbers)
-print(style.RED + 'We found ' + str(total_number) + ' numbers in the file' + style.RESET)
+total_number = len(numbers)
+print(style.RED + 'We found ' + str(total_number) +
+      ' numbers in the file' + style.RESET)
 delay = 30
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
@@ -63,28 +71,34 @@ print('Once your browser opens up sign in to web whatsapp')
 driver.get('https://web.whatsapp.com')
 input(style.MAGENTA + "AFTER logging into Whatsapp Web is complete and your chats are visible, press ENTER..." + style.RESET)
 for idx, number in enumerate(numbers):
-	number = number.strip()
-	if number == "":
-		continue
-	print(style.YELLOW + '{}/{} => Sending message to {}.'.format((idx+1), total_number, number) + style.RESET)
-	try:
-		url = 'https://web.whatsapp.com/send?phone=' + number + '&text=' + message
-		sent = False
-		for i in range(3):
-			if not sent:
-				driver.get(url)
-				try:
-					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='compose-btn-send']")))
-				except Exception as e:
-					print(style.RED + f"\nFailed to send message to: {number}, retry ({i+1}/3)")
-					print("Make sure your phone and computer is connected to the internet.")
-					print("If there is an alert, please dismiss it." + style.RESET)
-				else:
-					sleep(1)
-					click_btn.click()
-					sent=True
-					sleep(3)
-					print(style.GREEN + 'Message sent to: ' + number + style.RESET)
-	except Exception as e:
-		print(style.RED + 'Failed to send message to ' + number + str(e) + style.RESET)
+    number = number.strip()
+    if number == "":
+        continue
+    print(style.YELLOW + '{}/{} => Sending message to {}.'.format((idx+1),
+          total_number, number) + style.RESET)
+    try:
+        url = 'https://web.whatsapp.com/send?phone=' + number + '&text=' + message
+        sent = False
+        for i in range(3):
+            if not sent:
+                driver.get(url)
+                try:
+                    click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable(
+                        (By.XPATH, "//button[@data-testid='compose-btn-send']")))
+                except Exception as e:
+                    print(
+                        style.RED + f"\nFailed to send message to: {number}, retry ({i+1}/3)")
+                    print(
+                        "Make sure your phone and computer is connected to the internet.")
+                    print("If there is an alert, please dismiss it." + style.RESET)
+                else:
+                    sleep(1)
+                    click_btn.click()
+                    sent = True
+                    sleep(3)
+                    print(style.GREEN + 'Message sent to: ' +
+                          number + style.RESET)
+    except Exception as e:
+        print(style.RED + 'Failed to send message to ' +
+              number + str(e) + style.RESET)
 driver.close()
